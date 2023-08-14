5147ad5075bba0e768bf5a4dcc4e0e2341e93ecf
Randy Lopez <logentix@gmail.com>
2023-08-14 06:10:52 -0400

    added documentation for message_bus.py... need to use spinx to generate auto docs

--- engine/message_bus/message_bus.py
+++ engine/message_bus/message_bus.py
@@ -2,18 +2,44 @@ import multiprocessing
 
 
 class MessageBus:
+    """
+    Represents the message bus for inteprocess communication.
+
+    Attributes:
+        requests (multiprocessing.Queue): The requests channel.
+        events (multiprocessing.Queue): The events channel.
+    """
+
     def __init__(self):
         self.requests =  multiprocessing.Queue()
         self.events =  multiprocessing.Queue()
 
-    def subscribe(self, message_type):
-        if message_type == "request":
+    def subscribe(self, channel: str) ->  multiprocessing.Queue:
+        """
+        Subscribe to a specific channel.
+
+        Args:
+            channel (str): The specific channel to subscribe to.
+
+        Returns:
+            multiprocessing.Queue: A reference to the multiprocessing.Queue for the specified channel
+        """
+        if channel == "request":
             return self.requests
-        elif message_type == "event":
+        elif channel == "event":
             return self.events
 
-    def publish(self, message_type, message):
-        if message_type == "request":
+    def publish(self, channel: str, message) -> None:
+        """
+        Subscribe to a specific channel.
+
+        Args:
+            channel (str): The specific channel to publish to.
+
+        Returns:
+            None
+        """
+        if channel == "request":
             self.requests.put(message)
-        elif message_type == "event":
+        elif channel == "event":
             self.events.put(message)
